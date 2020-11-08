0a603686c877dd0aca12eeda46c9ac924a11d57e
Everson Silva <eversonsilva@outlook.com>
2020-11-07 22:05:38 -0300

    Testando integração contínua

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(accountsRouter);
-//require('dotenv').config();
+
 app.listen(process.env.PORT, () => console.log('API iniciada'));
